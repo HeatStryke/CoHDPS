9ea9499bd9136fe0b7ed85cc12e0ba259f6a8ec3
heatstryke <heatstryke@yahoo.com>
2024-10-12 13:54:15 -0400

    Some early work

--- Main.py
+++ Main.py
@@ -23,9 +23,20 @@ except Exception as e:
     print(f"An error occurred: {e}")  
 
 linecount-=1
-starth = int(lines[0][:2])
-startm = int(lines[0][:5][3:])
-starts = int(lines[0][:8][6:])
+x=0
+startx=0
+for line in lines:
+    if line.find('You hit')>0:
+         startx=line.find('You hit')
+         break
+         
+    if line.find('You hit')<=0:
+        x+=x
+
+
+starth = int(lines[startx][:2])
+startm = int(lines[startx][:5][3:])
+starts = int(lines[startx][:8][6:])
 
 
 starttime = datetime.datetime(1970,1,1,starth, startm,starts)
@@ -36,7 +47,8 @@ ends = int(lines[linecount][:8][6:])
 
 endtime = datetime.datetime(1970,1,1,endh,endm,ends)
 
-finaltime =  (endtime.minute * 60) + endtime.second
+
+finaltime =  (endtime.minute * 60)-(starttime.minute*60) + endtime.second - starttime.second
 print('Log time:', finaltime, 'seconds')
 
 x = 0
@@ -52,12 +64,25 @@ numattacks = 0
 x = 0
 for line in lines:
     if line[:7]=='You hit':
-        attacks.append(line[8:])
-        print(line[7:])
+        attacks.append(line[8:].strip())
         numattacks+=1
     if x > linecount:
         break
     x+=1
+numattacks-=1
 
 print('Number of Attacks:', numattacks)
+print('Actions per Second:',numattacks/finaltime)
+
 x=0
+temp=''
+stage2=[]
+for line in attacks:
+    temp=line[line.find('with your')+9:]
+    stage2.append(line[line.find('with your')+9:])
+    if x > numattacks:
+        break
+    x+=1
+
+x=0
+stage3=[]
